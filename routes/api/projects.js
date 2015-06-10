cf9398a6095819da362d13bc14ae74707e69174b
lucarin91 <lucarin91@gmail.com>
2015-06-10 21:38:12 +0200

    implement add item in project groups

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -28,19 +28,20 @@ router.get('/', function(req, res, next) {
     res.json(todos);
   });*/
   Projects.all.find({}).populate({path:'items'}) // only works if we pushed refs to children
-.exec(function (err, todos) {
-    if (err) return next(err);
-    //provvisorio capire come farlo fare a mongoDB
-    /*for (var i=0;i<todos.length;i++){
-      var items = todos[i].items;
-      for (var j=0;j<items.length;j++){
-        if (items[j].name)
-          items[j].name = items[j].name[req.lang];
-        if (items[j].info)
-          items[j].info = items[j].info[req.lang];
-      }
-    }*/
-    res.json(todos);
+    .exec(function (err, todos) {
+        if (err) return next(err);
+        //provvisorio capire come farlo fare a mongoDB
+        for (var i=0;i<todos.length;i++){
+          todos[i].name = todos[i].name[req.lang]
+          var items = todos[i].items;
+          for (var j=0;j<items.length;j++){
+            if (items[j].name)
+              items[j].name = items[j].name[req.lang];
+            if (items[j].info)
+              items[j].info = items[j].info[req.lang];
+          }
+        }
+        res.json(todos);
   });
 });
 
@@ -49,7 +50,7 @@ router.post('/',/* authController.isAuthenticated,*/ function(req, res, next) {
   if (!req.body.name) next(new Error('require name'));
   var data = {name: {} };
   data.name[req.lang]=req.body.name;
-  Projects.create(data, function (err, post) {
+  Projects.all.create(data, function (err, post) {
     if (err) return next(err);
     res.json(post._id);
   });
@@ -62,11 +63,17 @@ router.post('/:id',/* authController.isAuthenticated,*/ function(req, res, next)
   data.name[req.lang] = req.body.name;
   data.info[req.lang] = req.body.info;
   console.log(data);
-  Projects.update({_id:req.params.id},{$addToSet: {items: data}},{safe: true}, function(err,num,data){
+  Projects.item.create(data, function(err,post){
+    Projects.all.update({_id:req.params.id},{$addToSet: {items: post._id}},{safe: true}, function(err,num,data){
+      if (err) return next(err);
+      res.json(post._id);
+    });
+  });
+  /*Projects.update({_id:req.params.id},{$addToSet: {items: data}},{safe: true}, function(err,num,data){
     if (err) return next(err);
     var pictureIds = _.map(data, '_id');
     res.json(pictureIds);
-  });
+  });*/
 });
 
 /* DELETE /projects */
